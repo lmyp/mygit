e2649be79768a7693357ee8377c8ac9fb978aec5
lmyp <48020202+lmyp@users.noreply.github.com>
2019-05-29 22:16:28 +0800

    json数据
    JSON数据

---
+++ json数据/pages/index/index.js
@@ -0,0 +1,12 @@
+var jsonData = require('../../utils/json.js');
+Page({
+ data: {
+ },
+ //我们在这里加载本地json数据
+ onLoad: function () {
+  this.setData({
+   //jsonData.dataList获取json.js里定义的json数据，并赋值给dataList
+   dataList: jsonData.List
+  });  
+ },
+})
