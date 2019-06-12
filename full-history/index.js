17275a58bb06a541ce5e9675f1e2d0a5d2ae7fb5
lmyp <48020202+lmyp@users.noreply.github.com>
2019-06-12 21:36:25 +0800

    full-history
    full-history

---
+++ full-history/index.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var fs = require('fs');
+// 前端路由， spa 用户体验  快，没有白屏
+// router compoent 
+// 后端路由 http server /api 数据 ajax
+
+http.createServer(function (req, res) {
+    var filepath = '.' + req.url;
+    if(filepath === './') {
+        filepath = './index.html';
+    }
+    readFile(filepath, res);
+}).listen(80);
+function readFile( path, res) {
+    fs.readFile(path, 'utf-8', function(err, data) {
+        if(err) {
+            readFile('./index.html', res)
+            // throw new Error('出错了');
+        } else {
+            res.write(data);
+            res.end();
+        }
+    })
+}
---
+++ full-history/readme.md
@@ -0,0 +1,7 @@
+前端路由实现方式 history.pushState() 改变url的 path 部分， 跟hashChange实现方案，
+不一样的地方是改变的url部位不一样(#home),前端就可以处理路由了，
+2. 像极了后端路由,支持前后端切换
+3. 浏览历史， history onpopstate
+push pop 数组只在尾部操作， 栈
+
+数组推平
