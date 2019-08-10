93d0c60af1a810fcdc8aabff28ce3e5aa1df7e90
lmyp <48020202+lmyp@users.noreply.github.com>
2019-08-10 19:49:18 +0800

    utils
    utils

---
+++ xioamiyoupin/utils/mock.js
@@ -0,0 +1,188 @@
+[
+    {
+      "id":"guowei",
+      "banner":"/image/c1.png",
+      "cate":"鏋滃懗",
+      "detail":[
+        {
+          "thumb":"/image/c2.png",
+          "name":"姊ㄨ姳甯﹂洦 3涓�"
+        },
+        {
+          "thumb": "/image/c3.png",
+          "name": "绾㈡灒 500g"
+        },
+        {
+          "thumb": "/image/c4.png",
+          "name": "濂囧紓鏋� 2涓�"
+        },
+        {
+          "thumb": "/image/c4.png",
+          "name": "濂囧紓鏋� 2涓�"
+        },
+        {
+          "thumb": "/image/c3.png",
+          "name": "绾㈡灒 500g"
+        },
+        {
+          "thumb": "/image/c2.png",
+          "name": "姊ㄨ姳甯﹂洦 3涓�"
+        }
+      ]
+    },
+    {
+      "id": "shucai",
+      "banner": "/image/c1.png",
+      "cate": "钄彍",
+      "detail": [
+        {
+          "thumb": "/image/s4.png",
+          "name": "鐡滃瓙 3涓�"
+        },
+        {
+          "thumb": "/image/s5.png",
+          "name": "鑺硅彍 500g"
+        },
+        {
+          "thumb": "/image/s6.png",
+          "name": "灏忕背"
+        },
+        {
+          "thumb": "/image/s6.png",
+          "name": "灏忕背"
+        },
+        {
+          "thumb": "/image/s5.png",
+          "name": "鑺硅彍 500g"
+        },
+        {
+          "thumb": "/image/s4.png",
+          "name": "鐡滃瓙 4涓�"
+        }
+      ]
+    },
+    {
+      "id": "chaohuo",
+      "banner": "/image/c1.png",
+      "cate": "鐐掕揣",
+      "detail": [
+        {
+          "thumb": "/image/c2.png",
+          "name": "姊ㄨ姳甯﹂洦 3涓�"
+        },
+        {
+          "thumb": "/image/c3.png",
+          "name": "绾㈡灒 500g"
+        },
+        {
+          "thumb": "/image/c4.png",
+          "name": "濂囧紓鏋� 2涓�"
+        },
+        {
+          "thumb": "/image/c4.png",
+          "name": "濂囧紓鏋� 2涓�"
+        },
+        {
+          "thumb": "/image/c3.png",
+          "name": "绾㈡灒 500g"
+        },
+        {
+          "thumb": "/image/c2.png",
+          "name": "姊ㄨ姳甯﹂洦 3涓�"
+        }
+      ]
+    },
+    {
+      "id": "dianxin",
+      "banner": "/image/c1.png",
+      "cate": "鐐瑰績",
+      "detail": [
+        {
+          "thumb": "/image/s4.png",
+          "name": "鐡滃瓙 3涓�"
+        },
+        {
+          "thumb": "/image/s5.png",
+          "name": "鑺硅彍 500g"
+        },
+        {
+          "thumb": "/image/s6.png",
+          "name": "灏忕背"
+        },
+        {
+          "thumb": "/image/s6.png",
+          "name": "灏忕背"
+        },
+        {
+          "thumb": "/image/s5.png",
+          "name": "鑺硅彍 500g"
+        },
+        {
+          "thumb": "/image/s4.png",
+          "name": "鐡滃瓙 4涓�"
+        }
+      ]
+    },
+    {
+      "id": "cucha",
+      "banner": "/image/c1.png",
+      "cate": "绮楄尪",
+      "detail": [
+        {
+          "thumb": "/image/c2.png",
+          "name": "姊ㄨ姳甯﹂洦 3涓�"
+        },
+        {
+          "thumb": "/image/c3.png",
+          "name": "绾㈡灒 500g"
+        },
+        {
+          "thumb": "/image/c4.png",
+          "name": "濂囧紓鏋� 2涓�"
+        },
+        {
+          "thumb": "/image/c4.png",
+          "name": "濂囧紓鏋� 2涓�"
+        },
+        {
+          "thumb": "/image/c3.png",
+          "name": "绾㈡灒 500g"
+        },
+        {
+          "thumb": "/image/c2.png",
+          "name": "姊ㄨ姳甯﹂洦 3涓�"
+        }
+      ]
+    },
+    {
+      "id": "danfan",
+      "banner": "/image/c1.png",
+      "cate": "娣￠キ",
+      "detail": [
+        {
+          "thumb": "/image/s4.png",
+          "name": "鐡滃瓙 3涓�"
+        },
+        {
+          "thumb": "/image/s5.png",
+          "name": "鑺硅彍 500g"
+        },
+        {
+          "thumb": "/image/s6.png",
+          "name": "灏忕背"
+        },
+        {
+          "thumb": "/image/s6.png",
+          "name": "灏忕背"
+        },
+        {
+          "thumb": "/image/s5.png",
+          "name": "鑺硅彍 500g"
+        },
+        {
+          "thumb": "/image/s4.png",
+          "name": "鐡滃瓙 4涓�"
+        }
+      ]
+    }
+  ]
